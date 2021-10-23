334b737fd043100b6bb17e31dfafd2053e9a46df
Anderson Marcelino <anderson@andersonmarcelino.com.br>
2021-10-23 13:40:21 -0300

    remove gs prefix on set gs

--- src/d2cs/handle_d2cs.cpp
+++ src/d2cs/handle_d2cs.cpp
@@ -235,7 +235,6 @@ static int on_client_creategamereq(t_connection * c, t_packet * packet)
 	unsigned int	seqno, reply;
 	unsigned int	pos;
   unsigned int  game_to_find;
-  char * str_game_to_find;
 	t_elem		* elem;
 
 	pos=sizeof(t_client_d2cs_creategamereq);
@@ -293,10 +292,8 @@ static int on_client_creategamereq(t_connection * c, t_packet * packet)
 	} else if (!(game=d2cs_game_create(gamename,gamepass,gamedesc,gameflag))) {
 		reply=D2CS_CLIENT_CREATEGAMEREPLY_NAME_EXIST;
 	} else {
-    if(std::regex_match(gamedesc, std::regex("gs \\d"))) {
-      str_game_to_find = std::strtok((char *)gamedesc, " ");
-      str_game_to_find = std::strtok(NULL, " ");
-      game_to_find = (unsigned int)std::atoi(str_game_to_find);
+    if(std::regex_match(gamedesc, std::regex("^\\d$"))) {
+      game_to_find = (unsigned int)std::atoi((char *)gamedesc);
       manual_gs = d2gslist_find_gs(game_to_find);
       if (manual_gs != NULL) {
         gs = manual_gs;
